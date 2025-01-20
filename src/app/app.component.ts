b84d3a6759555e7337da796779409e840399d2a1
Celine Bouhafs <celine.b.dev@gmail.com>
2025-01-20 12:08:13 +0100

    Visual add & counter
    Mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component'
 import { CarouselComponent } from './carousel/carousel.component';
 import { DescriptionComponent } from './description/description.component';
+import { PurchaseComponent } from './purchase/purchase.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, CarouselComponent, DescriptionComponent],
+  imports: [NavigationComponent, CarouselComponent, DescriptionComponent, PurchaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
