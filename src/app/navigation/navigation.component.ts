f1993ceb843097e931b434d3c525fff46a6699ef
Celine Bouhafs <celine.b.dev@gmail.com>
2025-01-20 15:46:56 +0100

    Cart box
    Cart opening, visually working
    No item add WIP

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class NavigationComponent {
   isMenuOpen = false;
+  isCartOpen = false;
 
   openMenu(event: Event): void {
     event.stopPropagation();
@@ -20,5 +21,25 @@ export class NavigationComponent {
 
   closeMenu(): void {
     this.isMenuOpen = false;
+  }
+
+  toggleCart(event: Event): void {
+    event.stopPropagation(); // Empêche la propagation du clic pour que le HostListener ne ferme pas le panier
+    this.isCartOpen = !this.isCartOpen;
+    console.log('Panier ouvert:', this.isCartOpen); // Vérifiez l'état du panier
+  }
+
+  // Fermer le panier lorsqu'on clique en dehors
+  @HostListener('document:click', ['$event'])
+  closeCartOnOutsideClick(event: MouseEvent): void {
+    const cartElement = document.querySelector('.navbar__cart-content');
+    const cartButton = document.querySelector('.navbar__cart-button');
+    const target = event.target as HTMLElement;
+
+    // Si on clique en dehors du panier ET du bouton
+    if (cartElement && cartButton && !cartElement.contains(target) && !cartButton.contains(target)) {
+      this.isCartOpen = false;
+      console.log('Panier fermé'); // Vérifiez la fermeture
+    }
   }
 }
